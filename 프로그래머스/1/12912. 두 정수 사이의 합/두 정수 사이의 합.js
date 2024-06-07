2446bd0305abac90cd03c6be77d197901a68f061
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2024-06-07 10:08:56 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,16 @@
+function solution(a, b) {
+    if (a === b) {
+        return a;
+    }
+
+    if (a > b) {
+        [a, b] = [b, a];
+    }
+
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+
+    return sum;
+}
