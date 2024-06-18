6e9a1a4d41f3f03823f3a873f27b29359ba1562f
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2024-06-19 01:59:18 +0900

    [level 0] Title: 배열의 평균값, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,4 +1,4 @@
 function solution(numbers) {
-    var answer = numbers.reduce((a, b) => a + b) / numbers.length;
+    const answer = numbers.reduce((a,b) => a+ b) / numbers.length;
     return answer  
 }
