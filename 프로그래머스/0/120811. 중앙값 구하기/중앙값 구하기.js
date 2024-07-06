ba15c6275ff3d282a65664b058424569e2589c9e
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2024-07-06 21:30:40 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,3 +1,12 @@
 function solution(array) {
-    return array.sort((a,b)=> a - b)[Math.floor(array.length / 2)];
+    var answer = 0;
+    
+   
+    const p = array.sort((a, b) => a - b)
+    
+    const l =  Math.floor(p.length/2)
+    
+    const q = p[l]
+    
+    return q;
 }
