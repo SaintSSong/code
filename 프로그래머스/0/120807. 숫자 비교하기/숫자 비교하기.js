db170f57eefbf4713506a1b8ac69a81e2d0301a0
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2024-12-17 16:34:26 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,7 +1,5 @@
 function solution(num1, num2) {
-    if(num1 === num2) {
-        return  1
-    } else if( num1 !== num2) {
-        return -1
-    }
+let result = (num1 === num2) ? 1 : -1 ;
+    return result;
 }
+
