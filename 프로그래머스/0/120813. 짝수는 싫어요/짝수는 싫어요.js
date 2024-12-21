ce2d88067ea855b180b09ef19eef9726188497e8
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2024-12-21 21:39:47 +0900

    [level 0] Title: 짝수는 싫어요, Time: 3.94 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,12 +1,13 @@
 function solution(n) {
-    let result = []; // 결과를 저장할 배열
+    var answer = [];
     
-    // 1부터 n까지의 숫자를 순회합니다.
-    for (let i = 1; i <= n; i++) {
-        if (i % 2 !== 0) { // 숫자가 홀수인지 확인합니다.
-            result.push(i); // 홀수라면 결과 배열에 추가합니다.
+    for( let i = 1; i <= n; i++) {
+        if(i % 2 === 0) {
+            console.log("짝수입니다.")
+        } else {
+            console.log("홀수입니다.")
+            answer.push(i)
         }
     }
-    
-    return result; // 결과 배열을 반환합니다.
+    return answer;
 }
