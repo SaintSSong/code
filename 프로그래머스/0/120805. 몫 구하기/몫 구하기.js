d599d8841e5b24c92ecac99dadc7f1c95c835f52
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2025-02-08 23:42:00 +0900

    [level 0] Title: 몫 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
@@ -1,4 +1,7 @@
 function solution(num1, num2) {
-    const answer =  Math.floor(num1 / num2);
+    const answer =  parseInt(num1 / num2);
     return answer;
 }
+// 맨처음 num1 / num2로 했을 때 2번 예시가 3.5로 3이 아니여서 틀렸다.
+// 그러면 반올림 함수를 사용해서 정수만 나오게 하자.
+// Math.floor / parseInt 둘 다 사용 가능
