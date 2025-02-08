dad9330fa67ac5146063fba53f6f94c4d8eeec36
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2025-02-09 00:56:26 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,9 +1,17 @@
 function solution(array) {
+    const a = array
     
-
-    array.sort((a, b) => a - b);
+    const b = a.sort((a,b) => a - b)
     
-    let answer = Math.floor(array.length / 2)
+    const c = Math.floor(b.length / 2)
     
-    return array[answer];
+    const d = b[c]
+    
+    return d
 }
+
+
+// 중앙값? 정렬했을 떄? sort 쓰겠다.
+// 배열이 매개변수로 주어졌네? 그러면 그 배열을 정렬하고 그중에서 가장 가운데 인수를 뽑으면 되겠다
+// 그건 어떻게? 배열의 길이 / 2 
+// 근데 5개면 2.5인데 그걸 반올림 어떻게? 나눌때 반올림 해서 나눠주는 녀석을 쓰자.
