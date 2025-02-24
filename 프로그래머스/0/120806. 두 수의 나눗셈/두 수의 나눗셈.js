2ce7306e0e1acf8aa2e36313b438fad1eae86ff6
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2025-02-24 23:22:35 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -1,4 +1,10 @@
 function solution(num1, num2) {
-    var answer = Math.floor((num1/num2) * 1000)
-    return answer;
+    const g = Math.floor((num1 / num2)*1000)
+
+    return g;
 }
+// num1 과 num2를 나눈 값을 g라고한다.
+// g * 1000 = k 라고 한다.
+// 근데 곱한 후에 정수 부분이 있다고 한다. 
+// 정수 부분을 뽑는 메서드가 뭐지? 반올림? / 몫만 뽑는 것?
+// return k 를 쓰자. 
