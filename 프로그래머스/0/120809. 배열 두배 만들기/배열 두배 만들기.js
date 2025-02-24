15dc70c16714d57b5164635bd0f416d4b240cebc
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2025-02-24 23:26:20 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -1,4 +1,7 @@
 function solution(numbers) {
-    var answer = numbers.map((number) => number*2)
-    return answer;
+    var a = numbers.map((number) => number*2)
+    return a 
 }
+// 배열이 주어졌다.
+// 배열 속 원소에 각각 2배씩 곱해야한다.
+// 각 배열 속 원소를 찍을려면 map 함수다.
