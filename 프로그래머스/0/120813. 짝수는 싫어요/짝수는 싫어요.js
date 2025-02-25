f990ceb664df8d14aaa4b0dc487f16cfb494cff4
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2025-02-26 00:27:06 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,13 +1,18 @@
 function solution(n) {
-    var answer = [];
+    var a = [];
     
-    for( let i = 1; i <= n; i++) {
-        if(i % 2 === 0) {
-            console.log("짝수입니다.")
-        } else {
-            console.log("홀수입니다.")
-            answer.push(i)
+    for(i=0; i <= n; i++){
+        if(i % 2 !== 0) {
+            a.push(i)
         }
     }
-    return answer;
+    
+    return a;
 }
+
+// 배열을 return하네?
+// 홀수를 만드는 조건이 어떻게 될까?
+// k % 2 했는데 나머지가 나오면 홀수잖아.
+// 우선 정수 n보다 못한 숫자면 0부터 n까지 쭉 봐야하고 
+// 홀수가 나오면 그 숫자는 array.push해서 그걸 return해야겠다.
+
