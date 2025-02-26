e1296ec0fcfed099b9a09cb4261acee93dca7a7c
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2025-02-26 13:42:28 +0900

    [level 0] Title: 배열의 평균값, Time: 4.24 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,4 +1,18 @@
 function solution(numbers) {
-    const answer = numbers.reduce((a,b) => a+ b) / numbers.length;
-    return answer  
+
+    let count = 0;
+    
+    for(let num of numbers) {
+        count += num;
+        console.log("count", count)
+    }
+    
+    let answer = count / numbers.length; 
+    
+    return answer;
 }
+
+// 배열이 주어졌다.
+// 원소의 평균 값을 더하라고 하네?
+// 배열의 원소를 다 찍어봐야하네? 그러면 뭐가 있지? for of  
+// 그 후에 / 원소의 숫자만큼 해야하네 = 배열의 길이. array.length
