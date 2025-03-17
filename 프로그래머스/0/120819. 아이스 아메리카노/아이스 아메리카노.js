26960af323e9e185b8a4bd38ab69bf9af7b8374b
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2025-03-17 23:43:35 +0900

    [level 0] Title: 아이스 아메리카노, Time: 2.73 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,8 +1,24 @@
 function solution(money) {
-
-    const  coffee = Math.floor(money / 5500)
+    let m = money;
+    let coffee = 5500;
+    var answer = [];
     
-    const  restMoney = money % 5500
-          
-    return a = [coffee, restMoney];
+    // 잔수
+    let cup = Math.floor(m / coffee)
+    console.log("cup", cup)
+
+    answer[0] = cup
+    console.log("answer[0]", answer[0] )
+    
+    // 잔돈 
+    let other = m - (5500 * cup)
+    console.log("other", other )
+    
+    answer[1] = other
+    
+    return answer;
 }
+
+// 배열이다. 배열의 순서가 잔수, 남는 돈 순서대로 담아야하네? 그러면 arr[0] 이런 식으로 넣지 않을까?
+// 내가 배웠었던 반올림 함수가 뭐였지? (Math.ceil) 근데 그냥 Math.floor로 잔수를 넣어야겠다.
+// 남는 돈은 어떻게 계산하지? money - 5,500원 넣어야겠다.
