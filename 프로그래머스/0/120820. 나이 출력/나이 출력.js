b71fca9ce6717298a4427df4a0e9b7c36d884d35
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2025-03-17 23:47:49 +0900

    [level 0] Title: 나이 출력, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.js
@@ -0,0 +1,16 @@
+function solution(age) {
+    // 나이
+    let a = age;
+    
+    // 고정 값
+    let year = 2022;
+    
+    // 나이 구하기
+    let answer = year - a + 1;
+    
+    return answer;
+}
+
+
+// 22년 기준 나이.. 이거 뭐 어떻게 생각해야할까?
+// 2022 - 40
