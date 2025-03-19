fec71f4ab2021023cb46f4f22c95dfe7495f9f5c
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2025-03-20 01:04:52 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 5.34 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,16 +1,30 @@
 function solution(num_list) {
-    let evenCount = 0;  // 짝수 개수를 세는 변수
-    let oddCount = 0;   // 홀수 개수를 세는 변수
-
-    // 리스트의 각 원소를 순회합니다.
-    for (let i = 0; i < num_list.length; i++) {
-        if (num_list[i] % 2 === 0) {
-            evenCount++;  // 짝수이면 evenCount 증가
-        } else {
-            oddCount++;   // 홀수이면 oddCount 증가
+    let solo = [];
+    let double = [];
+    var answer = [];
+    
+        for(let i = 0; i < num_list.length; i++){
+            if(num_list[i] % 2 === 0) {
+                double.push(i);
+            } else {
+                solo.push(i);
+            }  
         }
-    }
 
-    // 결과를 배열로 반환합니다.
-    return [evenCount, oddCount];
+    let a = solo.length;
+    let b = double.length;
+    console.log(solo)
+    console.log(double)
+    console.log(a)
+    console.log(b)
+    
+    answer[0] = b;
+    answer[1] = a;
+
+    return answer;
 }
+
+// 배열이 주어지고 정수와 짝수의 개수를 담은 배열을 return하라?
+// 우선 num_list의 요소를 전부 돌아서 정수랑 짝수의 요소들만 따로 빼놔야겠네?
+// 그러고 그 요소들은 정수, 짝수라는 배열에 집어넣고
+// answer라는 배열에 "정수", "짝수"라는 배열의 갯수만 answer[] 이런 식으로 넣으면 되겠네
