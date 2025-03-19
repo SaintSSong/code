292539768313533b6019751c20fb115adaf9d49f
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2025-03-20 01:19:33 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 2.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,10 +1,22 @@
 function solution(my_string, n) {
-    var answer = '';
-    let b = my_string
+    var answer = [];
+    let a = my_string.split("")
     
-    for(let i = 0; i < b.length ; i++ ) {
-        answer += b[i].repeat(n)
+    
+    for (let i = 0; i < a.length; i++){
+         let aa =  my_string[i].repeat(n)
+        console.log(aa)
+        
+        answer += aa
+        
+        console.log(answer)
     }
-    
+
     return answer;
 }
+
+// 각 문자를 n 만큼 반복한다?
+// 우선 문자를 다 쪼개야겠네? split
+// 반복한다? repeat? 
+// 쪼개고 반복한걸 다시 집어넣는게 하나의 루틴이 되야해 즉 for문안에서 다 이루어져야하나?
+// join을 써야하나? +=은 안되나?
