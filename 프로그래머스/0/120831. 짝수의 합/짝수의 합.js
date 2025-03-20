1ed947b4ab5a6c68c3d6d024679e65a03007ffe0
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2025-03-21 00:50:04 +0900

    [level 0] Title: 짝수의 합, Time: 3.39 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -1,9 +1,16 @@
 function solution(n) {
-    let answer = 0;
-    for (let i = 1; i <= n; i++) {
-        if (i % 2 === 0) {
-            answer += i
-        }
+    var answer = 0;
+    let x = 0;
+    
+    console.log(n)
+    for (let i = 0; i <= n; i++){
+        if(i % 2 ===0) {
+            x += i
+        } 
     }
-    return answer;
+    
+    return x;
 }
+
+// n 이하의 짝수를 모두 더 한 값?
+// if문을 써서 let i = 0; 부터 해서 n까지 짝수를 구하고 그걸 다 +=해야겠네
