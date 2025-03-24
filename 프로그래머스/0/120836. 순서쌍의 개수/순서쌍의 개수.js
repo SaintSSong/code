86d6643f639845763d40a2cd52d505dd5841007b
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2025-03-25 00:42:26 +0900

    [level 0] Title: 순서쌍의 개수, Time: 5.67 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,10 +1,16 @@
 function solution(n) {
-let pairs = [];
-    for (let i = 1; i <= n; i++) {
-        if (n % i === 0) {
-            pairs.push([i, n / i]);
+    let count = 0;
+    
+    for (let i = 1; i <= n; i++){
+        if(n % i === 0){
+           count ++
         }
     }
-    let b = pairs.length
-    return b;
+
+    return count;
 }
+
+// 약수를 구하는거 같은데?? // 약수 구하는 방법이 따로 있나?
+// for문을 돌려서 1 ~ n까지 곱했을 때  n이 나오는 수 while도 되겠고
+
+    // 2중 for문??
