b4492e3b3d18f09dcfd79442d366e953714ad0c9
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2025-03-24 21:39:28 +0900

    [level 0] Title: 배열 자르기, Time: 3.23 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
@@ -1,6 +1,13 @@
 function solution(numbers, num1, num2) {
-    
-    const a = numbers.slice(num1,num2+1);
-    return a;
-    
+    let a = numbers;
+    let answer = a.slice(num1, num2 + 1);
+    console.log("num1", num1, "/ num2", num2);
+    console.log(answer)
+    return answer;
 }
+
+// 배열을 자르는 메서드가 있나?
+// for문을 돌리면 2중으로 돌려야하지 않을까?
+
+// slice? 메서드? 사용법은?
+// arr.slice("시작점", "끝점")
