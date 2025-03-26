01215ea98e9b0def55b6c9a9298f978763ca3496
SaintSSong <167057062+SaintSSong@users.noreply.github.com>
2025-03-26 10:35:55 +0900

    [level 0] Title: 가위 바위 보, Time: 6.23 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,33 +1,23 @@
 function solution(rsp) {
 
+    let sRSP = rsp.split("")
+    console.log("sRSP", sRSP)
     
-// 1. 숫자를 문자로 바꾼다.
-    let stringRsp = rsp.toString()
-    console.log("stringRsp", stringRsp)   
-// 2. 쪼갠다. split
-    let splitRsp = stringRsp.split('')
-    console.log("splitRsp", splitRsp)   
-
-    // map함수 안에서도 if - else if문을 쓸 수 있다.
-    // 배열로 전체 순회를 돈다.
-    let a = splitRsp.map((v) => 
-        {if(v === "2"){
-        return "0"}
-    else if(v === "0"){
-        return "5"}
-    else if(v === "5"){
-        return "2"
-    }})
+    let aRSP = sRSP.map((a) => {
+        if(a === "2") {
+            return "0"
+        } else if (a === "0"){
+            return "5"
+        } else if (a === "5") {
+            return "2"
+        }
+    }) 
     
-    console.log("a",a)
-
-    let answer = a.join("");
-    return answer;
+    console.log("aRSP", aRSP)
+    
+    let s = aRSP.join('')
+    return s;
 }
 
-// 지문 문자열을 나타내라.
-// 205 -> 052로 된다.
-// 1. 숫자를 문자로 바꾼다.
-// 2. 쪼갠다. split
-// 3. for문을 쓴다.
-// 4. 합친다. join 
+// 문자열 rsp
+// 문자열로 return 
